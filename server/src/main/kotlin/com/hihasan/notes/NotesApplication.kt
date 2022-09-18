66996d23168e9e8c008a90f4185ff2d1fdd0b667
Hasan Al Mamun <hihasan07@gmail.com>
2022-09-18 16:10:45 +0600

    Note Application Server

---
+++ server/src/main/kotlin/com/hihasan/notes/NotesApplication.kt
@@ -0,0 +1,11 @@
+package com.hihasan.notes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<NotesApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/hihasan/notes/NotesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hihasan.notes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
